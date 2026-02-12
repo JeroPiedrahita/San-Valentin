51e9e702c827cc2b23edd38ffd0a1497da1f107b
Jerónimo Piedrahita Franco <jeronimopiedrahita17@gmail.com>
2026-02-12 11:23:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const btnNo = document.querySelector("#btn-no");
+const btnSi = document.querySelector("#btn-si");
+const preAceptar = document.querySelector("#pre-aceptar");
+const postAceptar = document.querySelector("#post-aceptar");
 
+// Hacer que el botón "No" huya
+btnNo.addEventListener("mouseover", () => {
+    const x = Math.random() * (window.innerWidth - btnNo.offsetWidth);
+    const y = Math.random() * (window.innerHeight - btnNo.offsetHeight);
+    
+    btnNo.style.left = `${x}px`;
+    btnNo.style.top = `${y}px`;
+});
+
+// Acción al dar clic en "Sí"
+btnSi.addEventListener("click", () => {
+    preAceptar.classList.add("hidden");
+    postAceptar.classList.remove("hidden");
+});
